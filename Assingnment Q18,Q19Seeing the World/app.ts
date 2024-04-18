fc690d1a27cf1f82a46383cf64dd07293d788c2c
Psqasim <muhammadqasim0326@gmail.com>
2024-04-18 16:28:12 +0500

    45assingnement questions

--- Assingnment Q18,Q19Seeing the World/app.ts
+++ Assingnment Q18,Q19Seeing the World/app.ts
@@ -1,23 +1,65 @@
 //Q18
 //Seeing the World: Think of at least five places in the world you’d like to visit.
+
+
 // • Store the locations in a array. Make sure the array is not in alphabetical order.
-let dream = ["SAUDIA ARABIA", "JAPAN", "CHINA", "USA", "RUSSIA"];
+let dream : string[]= ["SAUDIA ARABIA","JAPAN","CHINA","USA","RUSSIA"]
+
 //Print your array in its original order.
+
 console.log(`orginal order :${dream}\n`);
+
 //• Print your array in alphabetical order without modifying the actual list.
+
 console.log(`alphabetical order:${[...dream].sort()}\n`);
+
 //• Show that your array is still in its original order by printing it.
+
 console.log(`original order: ${dream}\n`);
+
 //• Print your array in reverse alphabetical order without changing the order of the original list.
+
 console.log(`reverse alphabetical order without changing the order: ${[...dream].sort().reverse()}\n`);
+
+
 //• Show that your array is still in its original order by printing it again.
+
 console.log(`orginal order :${dream}\n`);
+
+
 //• Reverse the order of your list. Print the array to show that its order has changed.
+
 console.log(` Reverse the order of your list: ${[...dream].reverse()}\n`);
+
+
 //• Reverse the order of your list again. Print the list to show it’s back to its original order.
+
 console.log(` Reverse the order of your list: ${[...dream].reverse()}\n`);
+
 //• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
 console.log(`alphabetical order: ${dream.sort()}\n`);
+
+//.sort for arrangements just like a b c
+//.reverse for just like z y x
+
+
 //• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+
 console.log(`alphabetical order:${dream.sort().reverse()}\n`);
-export {};
+
+
+//Q19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let GuestList :string[] = ["TAYAAB AZIZ","ABDUL QAYYUM","IMTAIZ ALI","HASEEB"]
+
+// let i =0;
+
+// while (i<GuestList.length) {
+//     console.log(`Dear ${GuestList[i]},\n\nYou are cordially invited to dinner. It would be an honor to have your presence.\nSincerely,[M.QASIM]\n`);
+//     i++;
+    
+// }
+console.log(`The number of people you are inviting to dinner: ${GuestList.length}`);
