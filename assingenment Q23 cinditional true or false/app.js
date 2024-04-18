434ae5a290c02d7a2eee7a492bc28196923eda65
Psqasim <muhammadqasim0326@gmail.com>
2024-04-18 21:46:04 +0500

    45assingnement questions

---
+++ assingenment Q23 cinditional true or false/app.js
@@ -0,0 +1,34 @@
+//Q23
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//5true examples
+console.log(`5 true examples`);
+//1
+let car = "supraMk4";
+console.log(car === "supraMk4");
+//2
+console.log(car.length > 1);
+//3
+console.log(car !== "bmw");
+//4
+console.log(car.length >= 8);
+//5
+console.log(`${car.startsWith("s")}\n`);
+//false example
+console.log(`5 false example`);
+let cars = "BMWM5";
+//1
+console.log(cars === "supra");
+//2
+console.log(cars !== "BMWM5");
+//3
+console.log(cars.length < 4);
+//4
+console.log(cars.toLowerCase() === cars);
+//5
+console.log(cars.startsWith("s"));
+export {};
